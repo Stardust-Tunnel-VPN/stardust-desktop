f2eb3dd19f273c06d9809e8cb24f25ae8c92df56
paketoff <kekisreal228@gmail.com>
2025-04-07 13:56:06 +0200

    (main): Add first working version on macOS with some minor problems;

---
+++ electron/main.js
@@ -0,0 +1,76 @@
+const { app, BrowserWindow, session } = require("electron");
+const { spawn } = require("child_process");
+const path = require("path");
+
+let backendProcess;
+
+function startBackend() {
+  let backendPath;
+  if (process.platform === "win32") {
+    backendPath = path.join(__dirname, "..", "release", "backend", "win", "main.exe");
+  } else if (process.platform === "darwin") {
+    backendPath = path.join(__dirname, "..", "release", "backend", "mac", "main");
+  } else {
+    backendPath = path.join(__dirname, "..", "release", "backend", "linux", "main");
+  }
+
+  console.log("Launching backend from:", backendPath);
+  backendProcess = spawn(backendPath, [], {
+    detached: true,
+    stdio: "ignore",
+    shell: true, 
+  });
+  backendProcess.unref();
+}
+
+function setupRequestInterception() {
+  session.defaultSession.webRequest.onBeforeRequest({ urls: ["file://*/*"] }, (details, callback) => {
+    if (details.url.startsWith("file:///api/v1")) {
+      const newUrl = details.url.replace("file:///api/v1", "http://localhost:8000/api/v1");
+      console.log(`Redirecting: ${details.url} --> ${newUrl}`);
+      return callback({ redirectURL: newUrl });
+    }
+    return callback({});
+  });
+}
+
+function createWindow() {
+  const win = new BrowserWindow({
+    title: "Stardust VPN Client", 
+    width: 1024,
+    height: 768,
+    
+    icon: path.join(__dirname, "assets", "app_logo.png"),
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      // preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile(path.join(__dirname, "..", "release", "frontend", "dist", "index.html"));
+
+  win.webContents.openDevTools();
+
+  if (process.platform === "darwin") {
+    app.dock.setIcon(path.join(__dirname, "assets", "app_logo.png"));
+  }
+}
+
+app.whenReady().then(() => {
+  setupRequestInterception(); 
+  startBackend();
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
